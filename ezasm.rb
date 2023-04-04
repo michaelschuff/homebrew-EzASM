c55f89659e20585a102eb31e80b9ca26fbca16bc
michaelschuff <mschuff0881@gmail.com>
2023-04-04 17:21:53 -0400

    Version 0.0

--- ezasm.rb
+++ ezasm.rb
@@ -1,32 +1,20 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Ezasm < Formula
-  include Language::Python::Virtualenv
-
-  desc ""
-  homepage ""
+  desc "An assembly-like programming language for use in education"
+  homepage "https://github.com/ezasm-org/EzASM"
   url "https://github.com/michaelschuff/EzASM/archive/refs/tags/v0.0.2.tar.gz"
   sha256 "7bd482af01f44dfa2a76b3a643644f7fbf8a5843937bec1873c566af66e45c25"
-  license ""
+  license "MIT"
 
-  depends_on "python"
+  depends_on "maven" => :build
+  depends_on "openjdk@17"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    virtualenv_install_with_resources
+    system "mvn", "compile", "assembly:single"
+    libexec.install "target/EzASM-*-full.jar"
+    bin.write_jar_script libexec/"EzASM-*-full.jar", "ezasm", java_version: "17"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test ezasm`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "#{bin}/ezasm", "--version"
   end
 end
