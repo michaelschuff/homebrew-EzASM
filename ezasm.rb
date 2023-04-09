ea083c99d62cf29398ecc58a4dfd66f5f94d5ceb
michaelschuff <mschuff0881@gmail.com>
2023-04-09 17:45:21 -0400

    Version 0.0

--- ezasm.rb
+++ ezasm.rb
@@ -2,7 +2,7 @@ class Ezasm < Formula
   desc "An assembly-like programming language for use in education"
   homepage "https://github.com/ezasm-org/EzASM"
   url "https://github.com/michaelschuff/EzASM/archive/refs/tags/v1.3.1.tar.gz"
-  sha256 "7bd482af01f44dfa2a76b3a643644f7fbf8a5843937bec1873c566af66e45c25"
+  sha256 "43df4b64b635accff6b8057d0f8cf5d931883361813d2ff31175331768f5bb0c"
   license "MIT"
 
   depends_on "maven" => :build
@@ -10,7 +10,8 @@ class Ezasm < Formula
 
   def install
     system "mvn", "compile", "assembly:single"
-    exec("ls;cd target; ls")
+    libexec.install "target/EzASM-#{version}-beta-full.jar"
+    bin.write_jar_script libexec/"ezasm-0.0.2.jar", "ezasm", java_version: "17"
   end
 
   test do
