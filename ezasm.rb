02cbd952e9640aaa10376426fdcb846625df0cd9
michaelschuff <mschuff0881@gmail.com>
2023-04-09 17:58:46 -0400

    Version 0.0

--- ezasm.rb
+++ ezasm.rb
@@ -1,7 +1,7 @@
 class Ezasm < Formula
   desc "An assembly-like programming language for use in education"
   homepage "https://github.com/ezasm-org/EzASM"
-  url "https://github.com/michaelschuff/EzASM/archive/refs/tags/v1.3.1.tar.gz"
+  url "https://github.com/michaelschuff/EzASM/archive/refs/tags/v#{version}.tar.gz"
   sha256 "43df4b64b635accff6b8057d0f8cf5d931883361813d2ff31175331768f5bb0c"
   license "MIT"
 
